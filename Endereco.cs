84e9711c3cbd86d550ad4994210cca97b70b04ec
NeivaYang <yangaraujo.neiva@gmail.com>
2022-11-13 11:05:21 -0300

    New Features

--- Endereco.cs
+++ Endereco.cs
@@ -8,19 +8,12 @@ namespace Teste
     public class Endereco
     {
         public string? CEP {get;set;}
-
         public string? Logradouro {get;set;}
-
         public int? Numero {get;set;}
-
         public string? Complemento {get;set;}
-
         public string? Bairro {get;set;}
-
         public string? Cidade {get;set;}
-
         public string? Estado {get;set;}
-
         public bool? EnderecoComercial {get;set;}
 
     }
